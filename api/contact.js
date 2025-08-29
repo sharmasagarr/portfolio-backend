2bc542421f6b983c5241f4597dde8b9d32db04b7
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-08-29 10:59:20 +0530

    shift from nodemailer to resend due to authentication issue

--- api/contact.js
+++ api/contact.js
@@ -1,31 +1,25 @@
-import nodemailer from "nodemailer";
+// api/contact.js
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Only POST requests allowed" });
+  if (req.method === "POST") {
+    const { name, subject, email, message } = req.body;
+
+    try {
+      await resend.emails.send({
+        from: `Portfolio Contact <onboarding@resend.dev> - ${email}`,
+        to: "sharmasagar01@outlook.com",
+        subject: `Portfolio Contact Form: ${name} - ${subject}`,
+        text: message,
+      });
+
+      return res.status(200).json({ success: true, msg: "Message sent!" });
+    } catch (err) {
+      return res.status(500).json({ success: false, error: err.message });
+    }
   }
 
-  const { name, email, subject, message } = req.body;
-
-  try {
-    let transporter = nodemailer.createTransport({
-        service: "outlook", // or Outlook, Zoho, etc.
-        auth: {
-          user: process.env.EMAIL_USER, // from Vercel env vars
-          pass: process.env.EMAIL_PASS, // Outlook App Password
-        },
-    });
-
-    await transporter.sendMail({
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: `Portfolio Contact Form: ${name} - ${subject}`,
-      text: message,
-    });
-
-    res.status(200).json({ success: true });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ success: false, error: err.message });
-  }
+  res.status(405).json({ error: "Method not allowed" });
 }
