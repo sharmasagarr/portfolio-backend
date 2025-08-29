9ea6333e42f73ba6cc6e350c47dd528d582c8ad3
Sagar Sharma <102988240+sharmasagarr@users.noreply.github.com>
2025-08-29 12:03:15 +0530

    added cors configuration

--- api/contact.js
+++ api/contact.js
@@ -2,7 +2,26 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+// Allowed domains
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://sharmasagarr.github.io/portfolio/",
+];
+
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method === "POST") {
     const { name, subject, email, message } = req.body;
 
@@ -13,7 +32,7 @@ export default async function handler(req, res) {
         subject: `Portfolio Contact Form: ${subject}`,
         text: `From: ${name} <${email}>\n\nMessage:\n${message}`,
       });
-        
+
       return res.status(200).json({ success: true, msg: "Message sent!" });
     } catch (err) {
       return res.status(500).json({ success: false, error: err.message });
